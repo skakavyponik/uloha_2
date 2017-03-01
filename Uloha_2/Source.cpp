5963e9ec441a96884a6f780402dc4757f8d236a7
skakavyponik <j.bobosik@gmail.com>
2017-03-01 19:05:56 +0100

    treba sa spytat co je mozne pouzit intensity.val

--- Uloha_2/Source.cpp
+++ Uloha_2/Source.cpp
@@ -1,113 +1,51 @@
-/**
-* @file Morphology_1.cpp
-* @brief Erosion and Dilation sample code
-* @author OpenCV team
-*/
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
 
-#include "opencv2/imgproc.hpp"
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
 
+using namespace std;
 using namespace cv;
 
-/// Global variables
-Mat src, erosion_dst, dilation_dst;
 
-int erosion_elem = 0;
-int erosion_size = 0;
-int dilation_elem = 0;
-int dilation_size = 0;
-int const max_elem = 2;
-int const max_kernel_size = 21;
+int main() {
+	Mat image = imread("D:/Stiahnute/2.jpg");
+	int R[257] = { };
+	int G[257] = { };
+	int B[257] = { };
+	int sum=0;
 
-/** Function Headers */
-void Erosion(int, void*);
-void Dilation(int, void*);
+	for (int i = 0; i < image.rows; i++)
+		for (int j = 0; j < image.cols; j++)
+		{
+			Vec3b intensity = image.at<Vec3b>(Point(j, i));
+			int Red = intensity.val[0];
+			int Green = intensity.val[1];
+			int Blue = intensity.val[2];
+			R[Red] = R[Red] + 1;
+			G[Green] = G[Green] + 1;
+			B[Blue] = B[Blue] + 1;
+		}
+	Mat HistPlotR(750, 256, CV_8UC3, Scalar(0, 0, 0));
+	Mat HistPlotG(750, 256, CV_8UC3, Scalar(0, 0, 0));
+	Mat HistPlotB(750, 256, CV_8UC3, Scalar(0, 0, 0));
+	cout << "rozl " << image.cols*image.rows;
 
-/**
-* @function main
-*/
-int main(int, char** argv)
-{
-	/// Load an image
-	src = imread(argv[1], IMREAD_COLOR);
-
-	if (src.empty())
+	for (int i = 0; i < 256; i = i + 2)
 	{
-		return -1;
+		sum += R[i];
+		line(HistPlotR, Point(i, 750), Point(i, 750-R[i]/2), Scalar(0, 0, 255), 1, 8, 0);
+		line(HistPlotG, Point(i, 750), Point(i, 750-G[i]/2), Scalar(0, 255, 0), 1, 8, 0);
+		line(HistPlotB, Point(i, 750), Point(i, 750-B[i]/2), Scalar(255, 0, 0), 1, 8, 0);
 	}
 
-	/// Create windows
-	namedWindow("Erosion Demo", WINDOW_AUTOSIZE);
-	namedWindow("Dilation Demo", WINDOW_AUTOSIZE);
-	moveWindow("Dilation Demo", src.cols, 0);
-
-	/// Create Erosion Trackbar
-	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Erosion Demo",
-		&erosion_elem, max_elem,
-		Erosion);
-
-	createTrackbar("Kernel size:\n 2n +1", "Erosion Demo",
-		&erosion_size, max_kernel_size,
-		Erosion);
-
-	/// Create Dilation Trackbar
-	createTrackbar("Element:\n 0: Rect \n 1: Cross \n 2: Ellipse", "Dilation Demo",
-		&dilation_elem, max_elem,
-		Dilation);
-
-	createTrackbar("Kernel size:\n 2n +1", "Dilation Demo",
-		&dilation_size, max_kernel_size,
-		Dilation);
-
-	/// Default start
-	Erosion(0, 0);
-	Dilation(0, 0);
-
+	cout << "sum " << sum;
+	namedWindow("Red Histogram");
+	namedWindow("Green Histogram");
+	namedWindow("Blue Histogram");
+	imshow("Red Histogram", HistPlotR);
+	imshow("Green Histogram", HistPlotG);
+	imshow("Blue Histogram", HistPlotB);
 	waitKey(0);
 	return 0;
 }
-
-//![erosion]
-/**
-* @function Erosion
-*/
-void Erosion(int, void*)
-{
-	int erosion_type = 0;
-	if (erosion_elem == 0) { erosion_type = MORPH_RECT; }
-	else if (erosion_elem == 1) { erosion_type = MORPH_CROSS; }
-	else if (erosion_elem == 2) { erosion_type = MORPH_ELLIPSE; }
-
-	//![kernel]
-	Mat element = getStructuringElement(erosion_type,
-		Size(2 * erosion_size + 1, 2 * erosion_size + 1),
-		Point(erosion_size, erosion_size));
-	//![kernel]
-
-	/// Apply the erosion operation
-	erode(src, erosion_dst, element);
-	imshow("Erosion Demo", erosion_dst);
-}
-//![erosion]
-
-//![dilation]
-/**
-* @function Dilation
-*/
-void Dilation(int, void*)
-{
-	int dilation_type = 0;
-	if (dilation_elem == 0) { dilation_type = MORPH_RECT; }
-	else if (dilation_elem == 1) { dilation_type = MORPH_CROSS; }
-	else if (dilation_elem == 2) { dilation_type = MORPH_ELLIPSE; }
-
-	Mat element = getStructuringElement(dilation_type,
-		Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-		Point(dilation_size, dilation_size));
-
-	/// Apply the dilation operation
-	dilate(src, dilation_dst, element);
-	imshow("Dilation Demo", dilation_dst);
-}
-//![dilation]
