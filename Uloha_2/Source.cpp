752d04bce7487b7cecca7ba40fe00ec449b14026
skakavyponik <j.bobosik@gmail.com>
2017-03-01 19:40:29 +0100

    asi hotovo

--- Uloha_2/Source.cpp
+++ Uloha_2/Source.cpp
@@ -9,11 +9,13 @@ using namespace cv;
 
 
 int main() {
-	Mat image = imread("D:/Stiahnute/2.jpg");
-	int R[257] = { };
-	int G[257] = { };
-	int B[257] = { };
-	int sum=0;
+	Mat image = imread("D:/Stiahnute/3.jpg");
+	int R[257] = {};
+	int G[257] = {};
+	int B[257] = {};
+	int sum = 0;
+	int max = 1;
+	int div = 1;
 
 	for (int i = 0; i < image.rows; i++)
 		for (int j = 0; j < image.cols; j++)
@@ -31,12 +33,26 @@ int main() {
 	Mat HistPlotB(750, 256, CV_8UC3, Scalar(0, 0, 0));
 	cout << "rozl " << image.cols*image.rows;
 
-	for (int i = 0; i < 256; i = i + 2)
+	for (int i = 0; i < 256; i = i + 1)
+	{
+		if (R[i] > max)
+			max = R[i];
+		if (G[i] > max)
+			max = G[i];
+		if (B[i] > max)
+			max = B[i];
+
+	}
+
+	if (max < 750) max = 750;
+	div = max / 750;
+
+	for (int i = 0; i < 256; i = i + 1)
 	{
 		sum += R[i];
-		line(HistPlotR, Point(i, 750), Point(i, 750-R[i]/2), Scalar(0, 0, 255), 1, 8, 0);
-		line(HistPlotG, Point(i, 750), Point(i, 750-G[i]/2), Scalar(0, 255, 0), 1, 8, 0);
-		line(HistPlotB, Point(i, 750), Point(i, 750-B[i]/2), Scalar(255, 0, 0), 1, 8, 0);
+		line(HistPlotR, Point(i, 750), Point(i, 750-R[i]/div), Scalar(0, 0, 255), 1, 8, 0);
+		line(HistPlotG, Point(i, 750), Point(i, 750-G[i]/div), Scalar(0, 255, 0), 1, 8, 0);
+		line(HistPlotB, Point(i, 750), Point(i, 750-B[i]/div), Scalar(255, 0, 0), 1, 8, 0);
 	}
 
 	cout << "sum " << sum;
