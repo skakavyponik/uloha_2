cab60ccba27a7da582178840f717bc2056a20f7f
skakavyponik <j.bobosik@gmail.com>
2017-03-04 15:01:19 +0100

    plus obrazok aby bolo vidno

--- Uloha_2/Source.cpp
+++ Uloha_2/Source.cpp
@@ -8,7 +8,8 @@ using namespace std;
 using namespace cv;
 
 
-int main() {
+int main()
+{
 	Mat image = imread("D:/Stiahnute/3.jpg");
 	int R[257] = {};
 	int G[257] = {};
@@ -18,16 +19,20 @@ int main() {
 	int div = 1;
 
 	for (int i = 0; i < image.rows; i++)
+	{
 		for (int j = 0; j < image.cols; j++)
 		{
 			Vec3b intensity = image.at<Vec3b>(Point(j, i));
 			int Red = intensity.val[0];
 			int Green = intensity.val[1];
 			int Blue = intensity.val[2];
-			R[Red] = R[Red] + 1;
-			G[Green] = G[Green] + 1;
-			B[Blue] = B[Blue] + 1;
+			R[Red] += 1;
+			G[Green] += 1;
+			B[Blue] += 1;
 		}
+	}
+
+
 	Mat HistPlotR(750, 256, CV_8UC3, Scalar(0, 0, 0));
 	Mat HistPlotG(750, 256, CV_8UC3, Scalar(0, 0, 0));
 	Mat HistPlotB(750, 256, CV_8UC3, Scalar(0, 0, 0));
@@ -50,18 +55,19 @@ int main() {
 	for (int i = 0; i < 256; i = i + 1)
 	{
 		sum += R[i];
-		line(HistPlotR, Point(i, 750), Point(i, 750-R[i]/div), Scalar(0, 0, 255), 1, 8, 0);
-		line(HistPlotG, Point(i, 750), Point(i, 750-G[i]/div), Scalar(0, 255, 0), 1, 8, 0);
-		line(HistPlotB, Point(i, 750), Point(i, 750-B[i]/div), Scalar(255, 0, 0), 1, 8, 0);
+		line(HistPlotR, Point(i, 750), Point(i, 750 - R[i] / div), Scalar(0, 0, 255), 1, 8, 0);
+		line(HistPlotG, Point(i, 750), Point(i, 750 - G[i] / div), Scalar(0, 255, 0), 1, 8, 0);
+		line(HistPlotB, Point(i, 750), Point(i, 750 - B[i] / div), Scalar(255, 0, 0), 1, 8, 0);
 	}
 
-	cout << "sum " << sum;
+	cout << "\nsum " << sum;
 	namedWindow("Red Histogram");
 	namedWindow("Green Histogram");
 	namedWindow("Blue Histogram");
 	imshow("Red Histogram", HistPlotR);
 	imshow("Green Histogram", HistPlotG);
 	imshow("Blue Histogram", HistPlotB);
+	imshow("Picture", image);
 	waitKey(0);
 	return 0;
 }
